c4c4b6f263ff487b33e1b67a210948fd0c2ebe0b
hatch1010 <mikey51869@gmail.com>
2015-05-04 15:49:44 -0700

    Committing some code changes
    Code Changes. Probably very not important stuff

--- ThermostatTestApp/src/com/company/Main.java
+++ ThermostatTestApp/src/com/company/Main.java
@@ -1,22 +1,25 @@
 package com.company;
+import java.io.*;
 
 public class Main
 {
     //Initializing Objects
     private ComPortCommunication commPortCommunication = new ComPortCommunication();
     private RadioPktParsing radioPktParsing = new RadioPktParsing();
+    private BufferedReader bufferedReader;
     //Byte Arrays
-    private byte[] rawRadioPkt = new byte[64];
+    private byte[] rawRadioPkt = new byte[63];
     
 
     public static void main(String[] args)
     {
 	//write your code here
 
+
     }
     private void setRadioPktParsing()
     {
-        for(int i = 0; i<64; i++)
+        for(int i = 0; i<63; i++)
         {
             radioPktParsing.ReceivedRadioPkt[i] = rawRadioPkt[i];
             return;
